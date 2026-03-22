597526b41daed0f8dacf9314170d977e27cf7151
lukagbn <luka68690@gmail.com>
2026-03-22 17:14:54 +0400

    feat: add styles and password visibility toggle button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const form = document.querySelector("form");
 const password = document.getElementById("password");
 const email = document.getElementById("email");
+const message = document.querySelector(".message");
+const passVisible = document.querySelector(".pass-visible");
 
 async function login(data) {
   try {
@@ -10,13 +12,25 @@ async function login(data) {
       body: JSON.stringify(data),
     });
     const result = await res.json();
+    if (!res.ok) {
+      message.textContent = result.error;
+      message.classList.add("error");
+    } else {
+      message.textContent = result.message;
+      message.classList.add("success");
+    }
     console.log(result);
   } catch (error) {}
 }
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   login({ email: email.value, password: password.value });
 });
-password.addEventListener("input", () => {});
-email.addEventListener("input", () => {});
+passVisible.addEventListener("click", () => {
+  password.type = password.type === "password" ? "text" : "password";
+  if (password.type == "password") {
+    passVisible.textContent = "show";
+  } else {
+    passVisible.textContent = "hide";
+  }
+});
